5f57936ee3af2ef822c81a089f136b052f5221f8
rucha parab <rucha.parab@ajackus.com>
2021-06-17 18:25:29 +0530

    Added assets - javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,12 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "channels"
+import "jquery"
+
+import "packs/contacts.js"
