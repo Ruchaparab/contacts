5ee83b825bd863b3e6cf75fb7cf42bb21f41f2bb
rucha parab <rucha.parab@ajackus.com>
2021-06-17 21:02:56 +0530

    Added popper to webpack environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,8 @@ environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
   $: 'jquery/src/jquery',
   jQuery: 'jquery/src/jquery',
   jquery: 'jquery',
-  'window.jQuery': 'jquery'
+  'window.jQuery': 'jquery',
+  'Popper': ['popper.js', 'default']
 }))
 
 module.exports = environment
