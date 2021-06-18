47a5c38aa353ad9332a79e9809590e5b663c408c
rucha parab <rucha.parab@ajackus.com>
2021-06-18 10:40:38 +0530

    added missing validation for last name in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates_presence_of :first_name, :email, :message
+    validates_presence_of :first_name, :last_name, :email, :message
 
     def name
         "#{self.first_name} #{self.last_name}"
